ef71b70bc421fc861e57a91e2391c7fc68f35baf
Bugs Bunny <Cristian.e.cuevas707@gmail.com>
2021-06-30 19:56:35 -0400

    Added to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+
+    $(".saveBtn").on("click", function () {
+        var value = $(this).siblings(".description").val();
+        console.log(value);
+        var time = $(this).parent().attr("id");
+        console.log(time);
+        localStorage.setItem(time, value);
+    })
+
+    function getCurrentHour () {
+        var hour = moment().hours();
+        console.log(hour);
+
+        $(".time-block").each(function(){
+            var timeBlock = parseInt ($(this).attr('id').split('-')[1]);
+            console.log(timeBlock);
+            if (timeBlock < hour) {
+                $(this).addClass("past");
+            } 
+            else if (timeBlock === hour){
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        })
+    }
+    getCurrentHour();
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+    $("#hour-18 .description").val(localStorage.getItem("hour-18"));
+    
+// Shows current day
+function date (){
+var ecurrentDay = document.getElementById("currentDay");
+   var nowMoment = moment().format("dddd, MMMM Do");
+   ecurrentDay.innerHTML = nowMoment;
+}
+date();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
